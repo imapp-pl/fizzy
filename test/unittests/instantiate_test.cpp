27db94ce8d76fbdfc85be4cd7436c858cab3c01e
Andrei Maiboroda <andrei@ethereum.org>
2020-01-14 18:38:00 +0100

    Add instantiate tests for imported globals

--- test/unittests/instantiate_test.cpp
+++ test/unittests/instantiate_test.cpp
@@ -26,6 +26,44 @@ TEST(instantiate, imported_functions)
     EXPECT_EQ(instance.imported_function_types[0], TypeIdx{0});
 }
 
+TEST(instantiate, imported_globals)
+{
+    Module module;
+    module.importsec.emplace_back(Import{"mod", "g", ImportType::Global, {true}});
+
+    auto instance = instantiate(module, {}, {42});
+
+    ASSERT_EQ(instance.imported_globals_mutability.size(), 1);
+    EXPECT_EQ(instance.imported_globals_mutability[0], true);
+    ASSERT_EQ(instance.globals.size(), 1);
+    EXPECT_EQ(instance.globals[0], 42);
+}
+
+TEST(instantiate, imported_globals_multiple)
+{
+    Module module;
+    module.importsec.emplace_back(Import{"mod", "g1", ImportType::Global, {true}});
+    module.importsec.emplace_back(Import{"mod", "g2", ImportType::Global, {false}});
+
+    auto instance = instantiate(module, {}, {42, 43});
+
+    ASSERT_EQ(instance.imported_globals_mutability.size(), 2);
+    EXPECT_EQ(instance.imported_globals_mutability[0], true);
+    EXPECT_EQ(instance.imported_globals_mutability[1], false);
+    ASSERT_EQ(instance.globals.size(), 2);
+    EXPECT_EQ(instance.globals[0], 42);
+    EXPECT_EQ(instance.globals[1], 43);
+}
+
+TEST(instantiate, imported_globals_not_enough)
+{
+    Module module;
+    module.importsec.emplace_back(Import{"mod", "g1", ImportType::Global, {true}});
+    module.importsec.emplace_back(Import{"mod", "g2", ImportType::Global, {false}});
+
+    EXPECT_THROW(instantiate(module, {}, {42}), std::runtime_error);
+}
+
 TEST(instantiate, imported_functions_multiple)
 {
     Module module;
@@ -138,4 +176,19 @@ TEST(instantiate, globals_multiple)
     ASSERT_EQ(instance.globals.size(), 2);
     EXPECT_EQ(instance.globals[0], 42);
     EXPECT_EQ(instance.globals[1], 43);
+}
+
+TEST(instantiate, globals_with_imported)
+{
+    Module module;
+    module.importsec.emplace_back(Import{"mod", "g1", ImportType::Global, {true}});
+    module.globalsec.emplace_back(Global{true, GlobalInitType::constant, {42}});
+    module.globalsec.emplace_back(Global{false, GlobalInitType::constant, {43}});
+
+    auto instance = instantiate(module, {}, {41});
+
+    ASSERT_EQ(instance.globals.size(), 3);
+    EXPECT_EQ(instance.globals[0], 41);
+    EXPECT_EQ(instance.globals[1], 42);
+    EXPECT_EQ(instance.globals[2], 43);
 }
